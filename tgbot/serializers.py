6ed03cc89707bf2705c32fefc6d1641e7a0f404e
Maxim Dietrich <dietrich.maxim@gmail.com>
2024-10-12 15:30:59 +0400

    v.1.5.1 добавлены вложенные данные в сериалайзер

--- tgbot/serializers.py
+++ tgbot/serializers.py
@@ -2,13 +2,14 @@ from rest_framework.serializers import ModelSerializer
 from tgbot.models import Zacup, Botuser
 
 
+class BotuserSerializer(ModelSerializer):
+    class Meta:
+        model = Botuser
+        fields = ['id', 'user_surname', 'user_name', 'tg_id', 'tg_nic', 'date_of_registration', 'access_granted']
+
 class ZacupSerializer(ModelSerializer):
+    bot_user = BotuserSerializer()
     class Meta:
         model = Zacup
         fields = ['id', 'doc_number', 'status', 'booking_date', 'final_date', 'bot_user']
 
-
-class BotuserSerializer(ModelSerializer):
-    class Meta:
-        model = Botuser
-        fields = ['id', 'user_surname']
